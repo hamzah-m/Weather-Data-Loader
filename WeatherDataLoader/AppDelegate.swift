56aa727fc4d2a9a512a7ea0d0ec26ffbe9342ac8
Hamzah Mugharbil <Hamzah@hamzahs-mbp.lan>
2017-08-09 00:25:20 +0300

    Initial Commit

---
+++ WeatherDataLoader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherDataLoader
+//
+//  Created by Hamzah Mugharbil on 8/9/17.
+//  Copyright © 2017 Hamzah Mugharbil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WeatherDataLoader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherDataLoader
+//
+//  Created by Hamzah Mugharbil on 8/9/17.
+//  Copyright © 2017 Hamzah Mugharbil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
